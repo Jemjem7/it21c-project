5c0fd14d3ac43a877d3d185570710293b1a84e57
nyannncatt <kamirelax187@gmail.com>
2023-11-16 16:31:27 +0800

    Defined all foods class

---
+++ src/topic1_oop_basics/Food.java
@@ -0,0 +1,21 @@
+
+package topic1_oop_basics;
+
+
+public class Food {
+   String food_name;
+   double food_price;
+   
+ Food(String food_name,double food_price){
+     this.food_name = food_name;
+     this.food_price = food_price;
+     
+ }  
+ void showFoodName(){
+     System.out.println(food_name);
+ }
+ void showFoodPrice(){
+     System.out.println(food_price);
+ }
+}
+ 
--- src/topic1_oop_basics/Miain.java
+++ src/topic1_oop_basics/Miain.java
@@ -2,6 +2,11 @@ package topic1_oop_basics;
 
 public class Miain {
     public static void main(String[] args) {
-        System.out.println("OOP BASIC"); 
+    
+        Food food_01 = new Food ("siomeowers",40.00);
+       
+        food_01.showFoodName();
+        food_01.showFoodPrice();
 }
+    
 }
