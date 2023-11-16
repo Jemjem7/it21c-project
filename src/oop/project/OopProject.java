e8de316f4a2910197cc01a66a021ef546549a1bf
nyannncatt <kamirelax187@gmail.com>
2023-11-16 16:04:10 +0800

    added oop projects

---
+++ src/oop/project/OopProject.java
@@ -0,0 +1,11 @@
+
+package oop.project;
+
+public class OopProject {
+
+   
+    public static void main(String[] args) {
+        System.out.println("Package 1");
+    }
+    
+}
---
+++ src/topic1_oop_basics/Miain.java
@@ -0,0 +1,7 @@
+package topic1_oop_basics;
+
+public class Miain {
+    public static void main(String[] args) {
+        System.out.println("OOP BASIC"); 
+}
+}
